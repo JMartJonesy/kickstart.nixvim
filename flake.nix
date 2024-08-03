37f243a9283cf928ef75f31e704017455eb378dc
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-08-03 19:34:06 -0300

    Initialize nixvim standalone flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Nix/Nixvim implementation of kickstart.nvim";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    nixvim,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.legacyPackages.${system};
+        nixvimModule = {
+          inherit pkgs;
+          module = import ./config;
+          extraSpecialArgs = {
+          };
+        };
+        nvim = nixvim'.makeNixvimWithModule nixvimModule;
+      in {
+        checks = {
+          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        };
+
+        formatter = pkgs.alejandra;
+
+        packages = {
+          default = nvim;
+        };
+      };
+    };
+}
--- nixvim.nix
+++ nixvim.nix
@@ -1,15 +1,5 @@
-{ pkgs, inputs, ... }: {
+{pkgs, ...}: {
   imports = [
-    # NOTE: The first thing you will want to do is uncommented on of the three imports below
-    # depending on which module you chose to use to install Nixvim.
-    #
-    # Uncomment if you are using the home-manager module
-    #inputs.nixvim.homeManagerModules.nixvim
-    # Uncomment if you are using the nixos module
-    #inputs.nixvim.nixosModules.nixvim
-    # Uncomment if you are using the nix-darwin module
-    #inputs.nixvim.nixDarwinModules.nixvim
-
     # Plugins
     ./plugins/gitsigns.nix
     ./plugins/which-key.nix

77edb7224b95a9fd64555f64d28ff7d581fea6e5
jmartjonesy <jem4687@rit.edu>
2024-08-22 23:55:54 -0700

    Adding default providers for Wayland and X11

--- nixvim.nix
+++ nixvim.nix
@@ -162,10 +162,17 @@
       # Don't show the mode, since it's already in the statusline
       showmode = false;
 
-      # Sync clipboard between OS and Neovim
-      #  Remove this option if you want your OS clipboard to remain independent.
       #  See `:help 'clipboard'`
-      clipboard = "unnamedplus";
+      clipboard = {
+        providers = {
+          wl-copy.enable = true; # For Wayland
+          xsel.enable = true; # For X11
+        };
+
+        # Sync clipboard between OS and Neovim
+        #  Remove this option if you want your OS clipboard to remain independent.
+        register = "unnamedplus";
+      };
 
       # Enable break indent
       breakindent = true;

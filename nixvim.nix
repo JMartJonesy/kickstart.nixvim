4d653e3cb3ac50a72eeaef3229d874101157ab91
jmartjonesy <jem4687@rit.edu>
2024-08-22 23:58:09 -0700

    Quick fix to comment out home-manager nixvim import

--- nixvim.nix
+++ nixvim.nix
@@ -4,7 +4,7 @@
     # depending on which module you chose to use to install Nixvim.
     #
     # Uncomment if you are using the home-manager module
-    inputs.nixvim.homeManagerModules.nixvim
+    #inputs.nixvim.homeManagerModules.nixvim
     # Uncomment if you are using the nixos module
     #inputs.nixvim.nixosModules.nixvim
     # Uncomment if you are using the nix-darwin module

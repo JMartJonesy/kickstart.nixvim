db2f55c7756501e7684009f8142dee7b36545c14
auipga <auipga@gmail.com>
2025-03-11 12:08:36 +0100

    fix dap-ui and dap-go

--- plugins/kickstart/plugins/debug.nix
+++ plugins/kickstart/plugins/debug.nix
@@ -8,41 +8,41 @@
     # https://nix-community.github.io/nixvim/plugins/dap/index.html
     plugins.dap = {
       enable = true;
+    };
 
-      extensions = {
-        # Creates a beautiful debugger UI
-        dap-ui = {
-          enable = true;
+    # Creates a beautiful debugger UI
+    plugins.dap-ui = {
+      enable = true;
 
-          # Set icons to characters that are more likely to work in every terminal.
-          # Feel free to remove or use ones that you like more! :)
-          # Don't feel like these are good choices.
-          icons = {
-            expanded = "▾";
-            collapsed = "▸";
-            current_frame = "*";
-          };
-
-          controls = {
-            icons = {
-              pause = "⏸";
-              play = "▶";
-              step_into = "⏎";
-              step_over = "⏭";
-              step_out = "⏮";
-              step_back = "b";
-              run_last = "▶▶";
-              terminate = "⏹";
-              disconnect = "⏏";
-            };
-          };
+      # Set icons to characters that are more likely to work in every terminal.
+      # Feel free to remove or use ones that you like more! :)
+      # Don't feel like these are good choices.
+      settings = {
+        icons = {
+          expanded = "▾";
+          collapsed = "▸";
+          current_frame = "*";
         };
 
-        # Add your own debuggers here
-        dap-go = {
-          enable = true;
+        controls = {
+          icons = {
+            pause = "⏸";
+            play = "▶";
+            step_into = "⏎";
+            step_over = "⏭";
+            step_out = "⏮";
+            step_back = "b";
+            run_last = "▶▶";
+            terminate = "⏹";
+            disconnect = "⏏";
+          };
         };
       };
+    };
+
+    # Add your own debuggers here
+    plugins.dap-go = {
+      enable = true;
     };
 
     # https://nix-community.github.io/nixvim/keymaps/index.html

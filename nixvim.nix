f87384cdd9c2416dd77ee9a471b1beac45832cba
auipga <auipga@gmail.com>
2025-03-14 16:13:44 +0100

    Merge 'upstream': feat: add vim.opt.confirm = true

--- nixvim.nix
+++ nixvim.nix
@@ -215,6 +215,11 @@
       # Minimal number of screen lines to keep above and below the cursor.
       scrolloff = 10;
 
+      # if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+      # instead raise a dialog asking if you wish to save the current file(s)
+      # See `:help 'confirm'`
+      confirm = true;
+
       # See `:help hlsearch`
       hlsearch = true;
     };

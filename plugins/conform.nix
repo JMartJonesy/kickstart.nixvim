9518dba49bda2d9ef79c74bd40e02a8f44c04b92
mood <74420740+no-mood@users.noreply.github.com>
2025-04-04 18:50:25 +0200

    moved imports of plufins inside the nixvim option, formatted

--- plugins/conform.nix
+++ plugins/conform.nix
@@ -1,57 +1,55 @@
 {pkgs, ...}: {
-  programs.nixvim = {
-    # Dependencies
-    #
-    # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extrapackages
-    extraPackages = with pkgs; [
-      # Used to format Lua code
-      stylua
-    ];
+  # Dependencies
+  #
+  # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extrapackages
+  extraPackages = with pkgs; [
+    # Used to format Lua code
+    stylua
+  ];
 
-    # Autoformat
-    # https://nix-community.github.io/nixvim/plugins/conform-nvim.html
-    plugins.conform-nvim = {
-      enable = true;
-      settings = {
-        notify_on_error = false;
-        format_on_save = ''
-          function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for lanuages that don't
-            -- have a well standardized coding style. You can add additional
-            -- lanuages here or re-enable it for the disabled ones.
-            local disable_filetypes = { c = true, cpp = true }
-            return {
-              timeout_ms = 500,
-              lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
-            }
-          end
-        '';
-        formatters_by_ft = {
-          lua = ["stylua"];
-          # Conform can also run multiple formatters sequentially
-          # python = [ "isort "black" ];
-          #
-          # You can use a sublist to tell conform to run *until* a formatter
-          # is found
-          # javascript = [ [ "prettierd" "prettier" ] ];
-        };
+  # Autoformat
+  # https://nix-community.github.io/nixvim/plugins/conform-nvim.html
+  plugins.conform-nvim = {
+    enable = true;
+    settings = {
+      notify_on_error = false;
+      format_on_save = ''
+        function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for lanuages that don't
+          -- have a well standardized coding style. You can add additional
+          -- lanuages here or re-enable it for the disabled ones.
+          local disable_filetypes = { c = true, cpp = true }
+          return {
+            timeout_ms = 500,
+            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
+          }
+        end
+      '';
+      formatters_by_ft = {
+        lua = ["stylua"];
+        # Conform can also run multiple formatters sequentially
+        # python = [ "isort "black" ];
+        #
+        # You can use a sublist to tell conform to run *until* a formatter
+        # is found
+        # javascript = [ [ "prettierd" "prettier" ] ];
       };
     };
-
-    # https://nix-community.github.io/nixvim/keymaps/index.html
-    keymaps = [
-      {
-        mode = "";
-        key = "<leader>f";
-        action.__raw = ''
-          function()
-            require('conform').format { async = true, lsp_fallback = true }
-          end
-        '';
-        options = {
-          desc = "[F]ormat buffer";
-        };
-      }
-    ];
   };
+
+  # https://nix-community.github.io/nixvim/keymaps/index.html
+  keymaps = [
+    {
+      mode = "";
+      key = "<leader>f";
+      action.__raw = ''
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end
+      '';
+      options = {
+        desc = "[F]ormat buffer";
+      };
+    }
+  ];
 }

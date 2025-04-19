f4d593904bdf0319d153340716e0113a25705c36
auipga <auipga@gmail.com>
2025-04-19 05:06:37 +0200

    fix add 'untracked' gitsigns, change formatting

--- plugins/gitsigns.nix
+++ plugins/gitsigns.nix
@@ -7,11 +7,12 @@
       enable = true;
       settings = {
         signs = {
-          add = {text = "+";};
-          change = {text = "~";};
-          delete = {text = "_";};
-          topdelete = {text = "‾";};
-          changedelete = {text = "~";};
+          add.text = "+";
+          change.text = "~";
+          changedelete.text = "~";
+          delete.text = "_";
+          topdelete.text = "‾";
+          untracked.text = "┆";
         };
       };
     };

7e3e4a56bf5a9d4db40910600c342dc12e7d600f
auipga <auipga@gmail.com>
2025-04-20 03:28:14 +0200

    fix / remove hardcoded extraPlugin nvim-web-devicons, followup to 9c09e82d

--- nixvim.nix
+++ nixvim.nix
@@ -352,8 +352,6 @@ in
 
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extraplugins
     extraPlugins = with pkgs.vimPlugins; [
-      # Useful for getting pretty icons, but requires a Nerd Font.
-      nvim-web-devicons # TODO: Figure out how to configure using this with telescope
     ];
 
     # The line beneath this is called `modeline`. See `:help modeline`

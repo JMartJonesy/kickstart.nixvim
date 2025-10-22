df53a8c9456c7ae2df85ddbac33b0e9e36ad4b55
JMartJonesy <jem4687@rit.edu>
2025-10-21 21:30:00 -0700

    Revert "stop overriding the nixpkgs input for nixvim" as nixvim has fixed this bug on their side https://github.com/nix-community/nixvim/issues/3780#issuecomment-3369139566
    This reverts commit ece2cae2011003c607e5a8b9e002654c296d3ee1.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
